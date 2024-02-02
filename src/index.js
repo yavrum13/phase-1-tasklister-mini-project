898f6e7f5ddd021a4ebca7257a09c991c523a9f7
yavrum13 <yuval.avrum@gmail.com>
2024-02-02 13:02:16 -0600

    completed challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskInput = document.getElementById("new-task-description")
+  const submitButton = document.getElementById("create-task-form")
+  const taskList = document.getElementById("tasks")  
+
+  submitButton.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const taskText = taskInput.value;
+    console.log(taskText);
+
+    const listTask = document.createElement("li");
+    listTask.textContent = taskText;
+    taskList.appendChild(listTask);
+    taskInput.value = "";
+  })
 });
